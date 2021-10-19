6436b2cdc124639f63af7755ac53ee2d28228dde
Nathaniel <1735103@cegepmontpetit.ca>
2021-10-19 10:50:53 -0400

    FCT Menu Supprimer
    Ajout du code pour afficher le menu dans la main page

--- app/src/main/java/com/decoste/votedroid/MainActivity.java
+++ app/src/main/java/com/decoste/votedroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.decoste.votedroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.SupprimerQuestions:
+                Toast.makeText(getApplicationContext(), "Supprimer toutes les quetions selectionné", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.SupprimerVotes:
+                Toast.makeText(getApplicationContext(), "Supprimer tous les votes selectionné", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
