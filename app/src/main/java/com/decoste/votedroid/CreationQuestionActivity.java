adc5e1bf3a367f61ad605b9550584dcb58830c83
Nathaniel <1735103@cegepmontpetit.ca>
2021-10-27 15:06:58 -0400

    FCT Activiter Question
    Ajout du code de l'activité CréationQuestion

--- app/src/main/java/com/decoste/votedroid/CreationQuestionActivity.java
+++ app/src/main/java/com/decoste/votedroid/CreationQuestionActivity.java
@@ -1,14 +1,22 @@
 package com.decoste.votedroid;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.decoste.votedroid.databinding.ActivityCreationQuestionBinding;
+
 public class CreationQuestionActivity extends AppCompatActivity {
+    private ActivityCreationQuestionBinding binding;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityCreationQuestionBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
