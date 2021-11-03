01f4f809a0bc70482999afcd82077ffcfdd4d299
Nathaniel <1735103@cegepmontpetit.ca>
2021-11-03 15:32:07 -0400

    FCT xml pour l'activity vote.
    Ajout du code pour le layout de VoteActivity. (Non complet: manque les étoiles.)

--- app/src/main/java/com/decoste/votedroid/VoteActivity.java
+++ app/src/main/java/com/decoste/votedroid/VoteActivity.java
@@ -1,14 +1,22 @@
 package com.decoste.votedroid;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.decoste.votedroid.databinding.ActivityCreationQuestionBinding;
+import com.decoste.votedroid.databinding.ActivityVoteBinding;
+
 public class VoteActivity extends AppCompatActivity {
+    private ActivityVoteBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityVoteBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
