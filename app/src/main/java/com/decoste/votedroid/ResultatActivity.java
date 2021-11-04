30bc2bfc33681e968cf6171b70014cf399e31e15
Nathaniel <1735103@cegepmontpetit.ca>
2021-11-03 20:17:32 -0400

    FCT Chart pour ResultatActivity
    Ajout du code de la page ResultatActivity.

--- app/src/main/java/com/decoste/votedroid/ResultatActivity.java
+++ app/src/main/java/com/decoste/votedroid/ResultatActivity.java
@@ -1,22 +1,80 @@
 package com.decoste.votedroid;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.decoste.votedroid.databinding.ActivityCreationQuestionBinding;
 import com.decoste.votedroid.databinding.ActivityResultatBinding;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ResultatActivity extends AppCompatActivity {
     private ActivityResultatBinding binding;
 
+    private static final int MAX_X_VALUE = 5;
+    private static final int MAX_Y_VALUE = 4;
+    private static final int MIN_Y_VALUE = 0;
+    private static final String SET_LABEL = "Notes";
+
+    private BarChart chart;
+
+    private BarData createChartData() {
+        ArrayList<BarEntry> values = new ArrayList<>();
+        for (int i = 0; i < MAX_X_VALUE; i++) {
+            float x = i;
+            Random nb = new Random();
+            float y = nb.nextInt(MAX_Y_VALUE)-MIN_Y_VALUE;
+            values.add(new BarEntry(x, y));
+        }
+
+        BarDataSet set1 = new BarDataSet(values, SET_LABEL);
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        BarData data = new BarData(dataSets);
+
+        return data;
+    }
+
+    private void prepareChartData(BarData data) {
+        data.setValueTextSize(12f);
+        chart.setData(data);
+        chart.invalidate();
+    }
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityResultatBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        chart = view.findViewById(R.id.fragment_verticalbarchart_chart);
+
+        BarData data = createChartData();
+        configureChartAppearance();
+        prepareChartData(data);
     }
+
+    private void configureChartAppearance() {
+        chart.getDescription().setEnabled(false);
+        chart.setDrawValueAboveBar(false);
+
+    }
+
 }
